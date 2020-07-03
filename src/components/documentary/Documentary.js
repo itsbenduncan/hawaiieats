4b8d9f4263eb942b9a877a7c1a63f2663e7ab3d9
Ben Duncan <50156353+itsbenduncan@users.noreply.github.com>
2020-07-02 22:50:39 -1000

    some changes

--- src/components/documentary/Documentary.js
+++ src/components/documentary/Documentary.js
@@ -5,7 +5,13 @@ const Documentary = () => {
     return (
         <div className="documentary">
             <div className="coming-soon">
-                Coming Soon!
+                <h1>Coming Soon!</h1>
+            </div>
+            <div className="synopsis">
+                <span>
+                    Where does your dinner come from? A simple question, that leads to a complex and controversial topic.
+                    Now ask yourself, where does the dinner come from for the most isolated island chain in the world.
+                </span>
             </div>
         </div>
     )
--- src/components/resources/Resources.js
+++ src/components/resources/Resources.js
@@ -4,7 +4,37 @@ import './Resources.css';
 const Resources = () => {
     return (
         <div className="resources">
-        This is the RESOURCES page
+            <div className="resources-main">
+            <div className="producers-container">
+                    <h1>Local Producers</h1>
+                    <a id="body-link" href="https://gofarmhawaii.org/find-your-farmer/">Find a farmer</a>
+                </div>
+                <div className="orgs-container">
+                    <h1>Organizations</h1>
+                    <a id="body-link" href="https://www.kokuahawaiifoundation.org/aina">A`ina in the Schools (Kokua Foundation)</a>
+                </div>
+                <div className="policy-container">
+                    <h1>Policy Declorations</h1>
+                </div>
+                <div className="references">
+                    <h1>References</h1>
+                    <ul id="references-list">
+                        <li>1.	Schwartz, J. D. (2019). Water in plain sight: Hope for a thirsty world. New York: St. Martins Press.</li>
+                        <li>2.	Seufert, V., Ramankutty, N., & Mayerhofer, T. (2017). What is this thing called organic? – How organic farming is codified in regulations. Food Policy, 68, 10-20. doi:10.1016/j.foodpol.2016.12.009</li>
+                        <li>3.	Khan, S. S., Arita, S., Howitt, R., & Leung, P. (2018). A calibrated model of local food system of Hawaii: What are the economic implications of the states food goals and policies? Natural Resource Modeling, 32(1). doi:10.1111/nrm.12196</li>
+                        <li>4.	Loke, M. K., & Leung, P. (2013). Competing food concepts - Implications for Hawaii, USA. Food and Energy Security, 2(3), 174-184. doi:10.1002/fes3.33</li>
+                        <li>5.	Local Foods Production. (n.d.). Retrieved June 25, 2020, from <a id="reference-link" target="_blank" href="https://dashboard.hawaii.gov/stat/goals/5xhf-begg/vey8-efit/8pd6-pzhu">https://dashboard.hawaii.gov/stat/goals/5xhf-begg/vey8-efit/8pd6-pzhu</a></li>
+                        <li>6.	Winter, K., Beamer, K., Vaughan, M., Friedlander, A., Kido, M., Whitehead, A., . . . Nyberg, B. (2018). The Moku System: Managing Biocultural Resources for Abundance within Social-Ecological Regions in Hawaiʻi. Sustainability, 10(10), 3554. doi:10.3390/su10103554</li>
+                        <li>7.	Anonymous. Reducing Emissions from the Shipping Sector. 16 Feb. 2017, <a id="reference-link"target="_blank" href="https://ec.europa.eu/clima/policies/transport/shipping_en">ec.europa.eu/clima/policies/transport/shipping_en</a></li>
+                        <li>8.	Birch, L., Savage, J. S., & Ventura, A. (2007). Influences on the Development of Children's Eating Behaviours: From Infancy to Adolescence. Canadian journal of dietetic practice and research: a publication of Dietitians of Canada = Revue canadienne de la pratique et de la recherche en dietetique : une publication des Dietetistes du Canada, 68(1), s1–s56.</li>
+                        <li>9.	Kent, George. “Food Security in Hawai‘i.” Food and Power in Hawai'i, 2016, doi:10.21313/hawaii/9780824858537.003.0003.</li>
+                        <li>10.	Latest Population Estimate Data. May 2020, <a id="reference-link" target="_blank" href="https://census.hawaii.gov/home/population-estimate/">census.hawaii.gov/home/population-estimate/</a></li>
+                        <li>11.	MSU Food Literacy and Engagement Poll: Wave II. 8 Feb. 2019, <a id="reference-link" target="_blank" href="https://www.canr.msu.edu/news/msu-food-literacy-and-engagement-poll-wave-ii">www.canr.msu.edu/news/msu-food-literacy-and-engagement-poll-wave-ii</a></li>
+                        <li>12.	Ritchie, Hannah. Food Production Is Responsible for One-Quarter of the World's Greenhouse Gas Emissions. 6 Nov. 2019, <a id="reference-link" target="_blank" href="https://ourworldindata.org/food-ghg-emissions">ourworldindata.org/food-ghg-emissions</a></li>
+                        <li>13.	Ritchie, Hannah. You Want to Reduce the Carbon Footprint of Your Food? Focus on What You Eat, Not Whether Your Food Is Local. 24 Jan. 2020, <a id="reference-link" target="_blank" href="https://ourworldindata.org/food-choice-vs-eating-local">ourworldindata.org/food-choice-vs-eating-local</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
